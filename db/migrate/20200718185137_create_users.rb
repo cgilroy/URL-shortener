59521050ab396c87ebbc761310a83fdb2f323dc9
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2020-07-19 19:35:42 -0600

    Update users migration

--- db/migrate/20200718185137_create_users.rb
+++ db/migrate/20200718185137_create_users.rb
@@ -1,8 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      
+      t.string :email, null:false
       t.timestamps
     end
+    add_index :users, unique: true
   end
 end
