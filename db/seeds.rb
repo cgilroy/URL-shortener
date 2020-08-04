3b5e4d62d763ed7e5cb3da19f18ea619fca1efd8
CJ Gilroy <c.gilroy9@gmail.com>
2020-08-04 10:11:30 -0600

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user_1 = User.create!(email: 'connormcdavid@oilers.com')
+user_2 = User.create!(email: 'jeffbridges@gmail.com')
+short_url_1 = ShortenedUrl.create_short_url!(user_1,'www.lolcats.com')
+short_url_2 = ShortenedUrl.create_short_url!(user_1,'www.oilers.com')
+short_url_3 = ShortenedUrl.create_short_url!(user_2,'www.nhl.com')
+
+Visit.record_visit!(user_1,short_url_1)
+Visit.record_visit!(user_1,short_url_2)
+Visit.record_visit!(user_1,short_url_2)
+Visit.record_visit!(user_2,short_url_3)
+Visit.record_visit!(user_2,short_url_3)
+Visit.record_visit!(user_2,short_url_3)
+
+tag_topic_1 = TagTopic.create!(name:'Sports')
+tag_topic_2 = TagTopic.create!(name: 'Entertainment')
+
+Tagging.create!(shortened_url: short_url_1, tag_topic: tag_topic_2)
+Tagging.create!(shortened_url: short_url_2, tag_topic: tag_topic_1)
+Tagging.create!(shortened_url: short_url_3, tag_topic: tag_topic_1)
