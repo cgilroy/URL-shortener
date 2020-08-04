2f4dda6cf9c652a7e9e23263d004abc927f0c8bb
CJ Gilroy <c.gilroy9@gmail.com>
2020-08-04 10:11:48 -0600

    Add popular_links method

--- app/models/tag_topic.rb
+++ app/models/tag_topic.rb
@@ -11,4 +11,9 @@ class TagTopic < ApplicationRecord
         through: :taggings,
         source: :shortened_url
 
+    def popular_links
+        links = shortened_urls.sort_by { |url_obj| url_obj.num_clicks }.reverse
+        links.map { |url_obj| url_obj.long_url + ': ' + url_obj.num_clicks.to_s }.take(5)
+    end
+
 end
